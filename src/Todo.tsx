fcc2b5694a714ec3759b9ec401b36dc77da4bcb1
shwg <67375194+shwg8986@users.noreply.github.com>
2022-05-30 23:22:30 +0900

    Initial commit

---
+++ src/Todo.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: FC<Omit<TodoType, "id">> = (
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+  props
+) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完了]" : "[未完了]";
+  return <p>{`${completeMark} ${title}(ユーザー：${userId})`}</p>;
+};
